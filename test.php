ab4c83d7f3c3fe7676570e3d20b0829a9dcd6eb9
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-03 20:40:02 -0500

    test.php

--- test.php
+++ test.php
@@ -8,6 +8,8 @@
     $smtp = $mysqli->prepare("SELECT u.id_user, u.first_name FROM User u WHERE u.user_id = ?");
     var_dump(error_get_last());
     $smtp->bind_param("i", $id);
+        echo "test 1.5";
+    var_dump(error_get_last());
     $smtp->execute();
     $smtp->store_result();   
     var_dump(error_get_last()); 
