a7b7cd5191c437d1f7e7c592ae2af4ef7e10822c
Alfredo Hinojosa <alfhh4@gmail.com>
2015-10-04 00:13:50 -0500

    Add new product

---
+++ createProducto.php
@@ -0,0 +1,32 @@
+<?php include('./includes/conn.php');
+
+    $nameProducto = "";
+    $info = "";
+    $cost = "";
+
+    $nameProducto = $_POST['name'];
+    $info = $_POST['description'];
+    $cost = $_POST['amount'];
+
+    echo "\n " . $nameProducto . "\n";
+    echo "\n " . $info . "\n";
+    echo "\n " . $cost . "\n";
+
+    $mysqli = con_start();
+    $id_user = 1;
+
+    $smtp = $mysqli->prepare("INSERT INTO Product(id_user, name,
+              description, amount, completed, id_trans) VALUES (?,?,?,?,?,?) ");
+
+    $smtp->bind_param("issiii", $id_user, $nameProducto, $info, $cost, 1, 1);
+    $smtp->execute();
+
+    if (!$smtp->error) {
+        echo "correct"  ;
+    }
+    else {
+        echo "error" . $smtp->error;
+    }
+    $smtp->close();
+
+?>
