8a42dc41cfe7881283e4b1adc467d4ac6feb48c7
Luis Sanchez <luisjuansp@gmail.com>
2015-10-04 09:56:18 -0500

    Ajax

--- productoFunciones.php
+++ productoFunciones.php
@@ -10,10 +10,23 @@ $idprod = $_POST['idprod'];
 
 
 function addFav($id){
+
+	$smtp = $mysqli->prepare("UPDATE Product SET id_trans = 0 WHERE id_trans = 1");
+    $smtp->execute();
+
+    $smtp = $mysqli->prepare("UPDATE Product SET id_trans = 1 WHERE name like ?");
+    $smtp->bind_param("s",$id);
+    $smtp->execute();
+
+    $smtp->close();
 	echo "addFav" . $id;
 }
 
 function deleteProd($id){
+	$smtp = $mysqli->prepare("UPDATE Product SET hidden = 1 WHERE name like ?");
+    $smtp->bind_param("s",$id);
+    $smtp->execute();
+    $smtp->close();
 	echo "deleteProd" . $id;
 }
 
