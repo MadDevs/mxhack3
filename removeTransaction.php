8d37b0500bfed3cbf8274e28170bc18059ff5feb
mrquorr <sergio_93_op@hotmail.com>
2015-10-04 04:03:23 -0500

    checking

--- removeTransaction.php
+++ removeTransaction.php
@@ -4,7 +4,8 @@
 
     $return = 0;
 
-    echo "ss".$idt;
-    #$mysqli = con_start();
-    #$smtp = $mysqli->prepare("UPDATE  Tanda(id_user, name, intervalo_dias, num_personas, num_repeticiones, cantidad, fecha_inicial) VALUES (?,?,?,?,?,?,?) ");
+    $mysqli = con_start();
+    $smtp = $mysqli->prepare("UPDATE Transaction SET is_active=0 WHERE id_trans=?");
+    $smtp->bind_param($idt);
+    $smtp->execute();
 ?>
