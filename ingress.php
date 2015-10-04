1e2d752f679b4ad49523218aa116b69186822e4a
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-04 05:49:20 -0500

    cambios de cordero2

--- ingress.php
+++ ingress.php
@@ -156,8 +156,10 @@
             
             if ($.trim(json)!=0) {
               var addition = "<div class='row'>+ "+ amount+".00 <button class='remove' data-id='"+$.trim(json)+"' value='remove' style='color:red;'>Quitar ganancia</button></div>";
-              console.log(row);
-              row.append(row);
+              
+              console.log($(row));
+              row.append(addition);
+              $(row).append(addition);
               
              
             }
