0b4e04b6e6601c0b4a56069930b3e042070fe2c6
Alfredo Hinojosa <alfhh4@gmail.com>
2015-10-04 02:46:22 -0500

    worst fix v2

--- productoView.php
+++ productoView.php
@@ -35,24 +35,44 @@
     //var_dump($ret);
 
 ?>
-<h1>TEST</h1>
 <div class="container">
-    <table class="table">
-        <tr>
-            <th>Producto</th>
-            <th>Descripci&oacute;n</th>
-            <th>Costo</th>
-        </tr>
-        <?php
-        foreach($ret as $ok){
-            echo "<tr>";
-            echo "<td>".$ok[0]."</td>";
-            echo "<td>".$ok[1]."</td>";
-            echo "<td>".$ok[2]."</td>";
-            echo "</tr>";
-        }
-        ?>
-    </table>
-</div>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <h1>Nueva objetivo de compra</h1>
+    <h2>&iexcl;Quieres una nuevo art&iacute;culo, es momento de ahorrar!</h2>
+    <br>
+    <p class="bg-success" style="display: none; border-radius: 5px; text-align: center;" id="success">Producto agregado correctamente</p>
+    <p class="bg-error" style="display: none;   border-radius: 5px; text-align: center;" id="error">Error al agregar producto</p>
+    <form id="addProducto" name="newProduct" class="form-horizontal" action="createProducto.php" method="POST">
+        <div class="form-group">
+            <label for="nameProducto" class="col-sm-2 control-label">Nombre del art&iacute;culo</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="nameProducto" id="nameProducto" placeholder="Ejemplo: Lavadora, motocicleta, television..">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="description" class="col-sm-2 control-label">Descripci&oacute;n del art&iacute;culo</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="description" id="description"
+                       placeholder="Ejemplo: Es una televisi&oacute;n negra de 20 pulgadas marca LG">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="amount" class="col-sm-2 control-label">Costo $</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" step="50" name="amount" id="amount" placeholder="3,499">
+            </div>
+        </div>
+
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+
+        <button type="submit" class="btn btn-default">Guardar</button>
+
+    </form>
+</div><!-- /.container -->
 
 <?php  include("foot.php") ?>
