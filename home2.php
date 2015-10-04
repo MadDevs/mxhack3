e679f0617efd31d9e6284d50430dcbac253c0760
mrquorr <sergio_93_op@hotmail.com>
2015-10-03 20:23:43 -0500

    added card to first column

--- home2.php
+++ home2.php
@@ -11,6 +11,11 @@
 
     <title>Ingresos</title>
 
+    <!-- Google material css -->
+    <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.5/material.light_green-amber.min.css" />
+    <script src="https://storage.googleapis.com/code.getmdl.io/1.0.5/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
@@ -26,13 +31,31 @@
 </head>
 <body>
 
+<div class="row"></div>
 <div class="row">
   <div class="col-md-6">
-    <div class="row">Cada mes</div>
 
-    <div class="row">+ $4,000</div>
-    <div class="row">+ $3,500</div>
+    <div class="mdl-card mdl-shadow--2dp">
+      <div class="mdl-card__title mdl-card--expand">
 
+        <!-- title -->
+        <h2 class="mdl-card__title-text">Cada mes</h2>
+
+      </div>
+      <div class="mdl-card__supporting-text">
+
+        <!-- body -->
+        <div class="row">+ $4,000</div>
+        <div class="row">+ $3,500</div>
+
+      </div>
+      <div class="mdl-card__actions mdl-card--border">
+        <!-- button -->
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+          Agrega dinero
+        </a>
+      </div>
+    </div>
   </div>
 
   <div class="col-md-6">
@@ -44,7 +67,7 @@
     </div>
 
     <div class="row">
-      <div class="row">Enero 2015</div>
+      <div class="row">Febrero 2015</div>
       <div class="row">+ $50</div>
       <div class="row">+ $100</div>
       <div class="row">+ $100</div>
