961a09c592b81101d4298e8dd611dd86075f9fce
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-04 00:30:25 -0500

    producto

--- createProducto.php
+++ createProducto.php
@@ -1,25 +1,22 @@
 <?php include('./includes/conn.php');
 
-    $nameProducto = "";
-    $info = "";
-    $cost = "";
 
     $nameProducto = $_POST['name'];
     $info = $_POST['description'];
     $cost = $_POST['amount'];
 
-    echo "\n " . $nameProducto . "\n";
-    echo "\n " . $info . "\n";
-    echo "\n " . $cost . "\n";
 
     $mysqli = con_start();
     $id_user = 1;
 
-    $smtp = $mysqli->prepare("INSERT INTO Product(id_user, name,
-              description, amount, completed, id_trans) VALUES (?,?,?,?,?,?)");
-    echo "INSERT INTO Product(id_user, name,
-              description, amount, completed, id_trans) VALUES ($id_user, $nameProducto, $info, $cost, 1, 1)";
+    echo "</br> " . $nameProducto . "</br>";
+    echo "</br> " . $info . "</br>";
+    echo "</br> " . $cost . "</br>";
+    echo "</br> " . $id_user . "</br>";
 
+    $smtp = $mysqli->prepare("INSERT INTO Product(id_user, name, description, amount, completed, id_trans) VALUES (?,?,?,?,?,?)");
+    echo "INSERT INTO Product(id_user, name, description, amount, completed, id_trans) VALUES ($id_user, $nameProducto, $info, $cost, 1, 1)";
+    var_dump(error_get_last());
     $smtp->bind_param("issiii", $id_user, $nameProducto, $info, $cost, 1, 1);
     var_dump(error_get_last());
     $smtp->execute();
