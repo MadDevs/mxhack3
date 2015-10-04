88a8a4267cbc6e4d080c1fa73d2147dd292f9301
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-03 22:52:31 -0500

    test.php

--- createTanda.php
+++ createTanda.php
@@ -16,13 +16,14 @@
     echo "\n " . $cantidad . "\n";
     echo "\n " . $intervalo . "\n";
     echo "\n " . $nameTanda . "\n";
-    /*
+    echo date("Y-m-d");
+
     $mysqli = con_start();
     $ret = [];
     $count = 0;
 
     $id = 1;
-    $smtp = $mysqli->prepare("SELECT u.id_user, u.first_name FROM User u WHERE u.id_user = ?");
+    $smtp = $mysqli->prepare("INSERT INTO ");
     
     $smtp->bind_param("i", $id);
     $smtp->execute();
@@ -37,6 +38,6 @@
     $smtp->free_result();
     $smtp->close();
     var_dump($ret);
-    */
+    
 
   ?>  
