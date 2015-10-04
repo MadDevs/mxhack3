b9d24e4af9e37ebb109fcbe172f0b7676389abeb
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-03 20:38:54 -0500

    test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
   <?php include('./includes/conn.php'); 
-    echo "test";
+    echo "test 1";
     $mysqli = con_start();
     $ret = [];
     $count = 0;
@@ -8,18 +8,17 @@
     $smtp = $mysqli->prepare("SELECT u.id_user, u.first_name FROM User u WHERE u.user_id = ?");
     var_dump(error_get_last());
     $smtp->bind_param("i", $id);
-    
-    var_dump(error_get_last());
     $smtp->execute();
-    $smtp->store_result();    
-    var_dump(error_get_last());
+    $smtp->store_result();   
+    var_dump(error_get_last()); 
+    echo "test 2";
     $smtp->bind_result($id, $first_name);
     while ($smtp->fetch()) {
          $ret[$count][0] =  $id;
          $ret[$count][1] =  $first_name;
          $count++;
     }
-    var_dump(error_get_last());
+    echo "test 3";
     $smtp->free_result();
     $smtp->close();
     var_dump($ret);
