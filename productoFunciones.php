d2ddfc82ebf9205c678dca6be790b549db42574e
Luis Sanchez <luisjuansp@gmail.com>
2015-10-04 09:58:33 -0500

    Ajax

--- productoFunciones.php
+++ productoFunciones.php
@@ -6,11 +6,9 @@ $mysqli = con_start();
 
 
 $funcion = $_POST['funcion'];
-$idprod = $_POST['idprod'];
+$id = $_POST['idprod'];
 
-
-function addFav($id){
-
+if($funcion == "addFav"){
 	$smtp = $mysqli->prepare("UPDATE Product SET id_trans = 0 WHERE id_trans = 1");
     $smtp->execute();
 
@@ -20,21 +18,12 @@ function addFav($id){
 
     $smtp->close();
 	echo "addFav" . $id;
-}
-
-function deleteProd($id){
+}elseif ($funcion == "deleteProd") {
 	$smtp = $mysqli->prepare("UPDATE Product SET hidden = 1 WHERE name like ?");
     $smtp->bind_param("s",$id);
     $smtp->execute();
     $smtp->close();
 	echo "deleteProd" . $id;
-}
-
-
-if($funcion == "addFav"){
-	addFav($idprod);
-}elseif ($funcion == "deleteProd") {
-	deleteProd($idprod);
 }
 
 ?>
