ff3bf0c3958b6376da4121a05b4419678f86229e
Enrique Hernandez <enriqueohernandez@gmail.com>
2015-10-04 11:02:30 -0500

    Nueva deuda

--- nuevaDeudaControlador.php
+++ nuevaDeudaControlador.php
@@ -21,6 +21,7 @@ $mysqli = con_start();
 
 //echo $name.$description.$quantity;
 
+
 $smtp = $mysqli->prepare("INSERT INTO Deudores (id_user, name, description, amount)
       VALUES(?,?,?,?)");
 
@@ -33,6 +34,26 @@ $smtp->execute();
 
 //echo"ejecute2";
 $smtp->free_result();
+$smtp->close();
+
+
+//$time = date("Y-m-d");
+$type = 2;
+$created = date("Y-m-d");
+$nombremejor =  $nombre." pidio prestado";
+$monthly = 1;
+
+// echo "entro a pagar";
+$mysqli = con_start();
+//echo "entro a pagar";
+$smtp = $mysqli->prepare("INSERT INTO Transaction (id_user, type, amount, monthly, created, description) VALUES(?,?,?,?,?,?)");
+//echo "entro a pagar";
+$smtp->bind_param("iiiiis",$id_usuario, $type, $quantity, $monthly,  $created, $nombremejor);
+//echo "entro a pagar";
+$smtp->execute();
+//echo "entro a pagar";
+$smtp->free_result();
+// echo "entro a pagar";
 $smtp->close();
 
 header("Location: deudas.php");
