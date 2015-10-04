a5aede46f6ea71e7d9761633e860a00e12063225
mrquorr <sergio_93_op@hotmail.com>
2015-10-04 03:53:39 -0500

    checking

---
+++ removeTransaction.php
@@ -0,0 +1,10 @@
+<?php include('./includes/conn.php');
+
+    $idt = $_POST['id'];
+
+    $return = 0;
+
+    echo "ss".$idt;
+    #$mysqli = con_start();
+    #$smtp = $mysqli->prepare("UPDATE  Tanda(id_user, name, intervalo_dias, num_personas, num_repeticiones, cantidad, fecha_inicial) VALUES (?,?,?,?,?,?,?) ");
+?>
