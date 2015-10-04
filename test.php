090ec424d89881d8aa47a0f2ad262543cc8b9fb6
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-03 20:41:40 -0500

    test.php

--- test.php
+++ test.php
@@ -5,14 +5,12 @@
     $count = 0;
 
     $id = 1;
-    $smtp = $mysqli->prepare("SELECT u.id_user, u.first_name FROM User u WHERE u.user_id = ?");
-    var_dump(error_get_last());
-    $smtp->bind_param("i", $id);
-        echo "test 1.5";
-    var_dump(error_get_last());
+    $smtp = $mysqli->prepare("SELECT u.id_user, u.first_name FROM User u WHERE u.user_id = " . $id);
+    
+
     $smtp->execute();
     $smtp->store_result();   
-    var_dump(error_get_last()); 
+     
     echo "test 2";
     $smtp->bind_result($id, $first_name);
     while ($smtp->fetch()) {
