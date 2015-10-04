331e431e4f950b447b837d2f34ec3897b2d126c2
Alfredo Hinojosa <alfhh4@gmail.com>
2015-10-04 02:27:23 -0500

    Fix blank lines

--- createProducto.php
+++ createProducto.php
@@ -7,21 +7,11 @@
     $nameProducto = $_POST['nameProducto'];
     $info = $_POST['description'];
     $cost = $_POST['amount'];
-
-
-
     $mysqli = con_start();
     $id_user = 1;
-
-
-
     $smtp = $mysqli->prepare("INSERT INTO Product(id_user, name, description, amount, completed, id_trans) VALUES (?,?,?,?,?,?)");
-
     $smtp->bind_param("issiii", $id_user, $nameProducto, $info, $cost, $id_user, $id_user);
-
     $smtp->execute();
-
-
     if (!$smtp->error) {
         echo "correct"  ;
     }
