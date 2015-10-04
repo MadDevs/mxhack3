ffe69f303dcb32dce6cdbaafb9f3f57b387ebc87
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-04 02:17:28 -0500

    producto

--- tandas.php
+++ tandas.php
@@ -1,66 +1,9 @@
 
+
 <?php
-/**
- * Created by PhpStorm.
- * User: enriqueohernandez
- * Date: 10/3/15
- * Time: 7:04 PM
- */
-
+    $titulo = "Tandas";
+    include ("head.php")
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Tandas</title>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-
-
-    <!-- Custom styles for this template -->
-    <link href="css/cover.css" rel="stylesheet">
-
-</head>
-
-
-<body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div>
-    </nav>
-
 
 
 
@@ -117,7 +60,7 @@
             <div class="form-group">
                 <label for="Intervalo" class="col-sm-2 control-label">Tu turno de Tanda</label>
                 <div class="col-sm-10">
-                    <select class="form-control" id="turno" name="turno">
+                    <select class="form-control" id="turnoPersona" name="turnoPersona">
                         <option value="1" selected>1</option>
                     </select>
                 </div>
@@ -211,8 +154,7 @@ $('#addTanda').on('submit', function (e) {
 
 </div><!-- /.container -->
 
-</body>
-</html>
+<?php  include("foot.php") ?>
 
 
 
