e30f236d3b917ee0151d0d3e717d34e40cc6e29d
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-04 01:16:07 -0500

    tandas

--- tandas.php
+++ tandas.php
@@ -152,11 +152,11 @@
             $("#personas").empty();
             var strDiv = "";
             for (var i = 1; i <= number && i < 9; i++) {
-                strDiv += "<label for=\"namePersona[]\" class=\"col-sm-2 control-label\">Nombre " + i + " </label>";
-                strDiv += "<div class=\"col-sm-6\">";
-                strDiv += "<input type=\"text\" class=\"form-control\" name=\"namePersona[]\" id=\"namePersona\" placeholder=\"Nombre de la Persona " + i + "\">";
-                strDiv += "</div><label for=\"namePersona[]\" class=\"col-sm-1 control-label\">Turno</label>";
-                strDiv += "<div class=\"col-sm-3\"><select class=\"form-control\" id=\"turno" + i + "\" name=\"turno\"></select>";
+                strDiv += "<label for='namePersona[]' class='col-sm-2 control-label'>Nombre " + i + " </label>";
+                strDiv += "<div class='col-sm-6'>";
+                strDiv += "<input type='text' class='form-control' name='namePersona[]' id='namePersona' placeholder='Nombre de la Persona " + i + "'>";
+                strDiv += "</div><label for='namePersona[]' class='col-sm-1 control-label'>Turno</label>";
+                strDiv += "<div class='col-sm-3'><select class='form-control' id='turno" + i + "' name='turno[]'></select>";
                 strDiv += "</div><br><br>";
             };
             $("#personas").append(strDiv);
@@ -174,7 +174,7 @@
 
 
 
-$('#addTanda').on('submit', function (e) {
+$('addTanda').on('submit', function (e) {
 
   e.preventDefault();
 
