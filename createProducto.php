db5c88e24cfbf8ef87ebd6c14525a174fd8ec71f
Alfredo Hinojosa <alfhh4@gmail.com>
2015-10-04 03:17:49 -0500

    Changed the not completed

--- createProducto.php
+++ createProducto.php
@@ -9,8 +9,10 @@
     $cost = $_POST['amount'];
     $mysqli = con_start();
     $id_user = 1;
+    $notCompleted = 0;
+
     $smtp = $mysqli->prepare("INSERT INTO Product(id_user, name, description, amount, completed, id_trans) VALUES (?,?,?,?,?,?)");
-    $smtp->bind_param("issiii", $id_user, $nameProducto, $info, $cost, $id_user, $id_user);
+    $smtp->bind_param("issiii", $id_user, $nameProducto, $info, $cost, $notCompleted, $id_user);
     $smtp->execute();
     if (!$smtp->error) {
         echo "correct"  ;
