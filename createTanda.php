7387057471afe8f89f4892ef2a8140eb0c653c3d
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-04 03:17:38 -0500

    test

--- createTanda.php
+++ createTanda.php
@@ -13,7 +13,7 @@
 
     $return = 0;
 
-    var_dump("expression");
+    
 
     $mysqli = con_start();
     $smtp = $mysqli->prepare("INSERT INTO Tanda(id_user, turno, name, intervalo_dias, num_personas, num_repeticiones, cantidad, fecha_inicial) VALUES (?,?,?,?,?,?,?) ");
@@ -27,15 +27,15 @@
     }
     $smtp->close();
     
+
+    var_dump(error_get_last());
     $id_tanda = 1;
     // Id de tanda que se acaba de insertar
     $mysqli = con_start();
     $smtp = $mysqli->prepare("SELECT id_tanda FROM Tanda ORDER BY id_tanda DESC LIMIT 1");
     $smtp->execute();
-
     $smtp->store_result();   
     $smtp->bind_result($id_tand);
-
     while ($smtp->fetch()) {
         $id_tanda = $id_tand;
     }
@@ -44,6 +44,7 @@
 
 
 
+    var_dump(error_get_last());
     foreach( $namePersona as $key => $value ) {
       $mysqli = con_start();
 
