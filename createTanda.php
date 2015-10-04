4bf6edfdce1c72f6ed78ec41068a29b068a4eeb7
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2015-10-03 23:20:57 -0500

    tandas.php

--- createTanda.php
+++ createTanda.php
@@ -16,9 +16,10 @@
 
     $mysqli = con_start();
     $id_user = 1;
+
     $smtp = $mysqli->prepare("INSERT INTO Tanda(id_user, name, intervalo_dias, num_personas, num_repeticiones, cantidad, fecha_inicial) VALUES (?,?,?,?,?,?,?) ");
     
-    $smtp->bind_param("isiiiis", $id_user, $intervalo, $numPeople, $numRep, $cantidad, $today);
+    $smtp->bind_param("isiiiis", $id_user,$nameTanda, $intervalo, $numPeople, $numRep, $cantidad, $today);
     $smtp->execute();
  
     if (!$smtp->error) {
